3478ad18d882594b4f52bda26669fc0ec3012a43
Isaba Ishrak <isabaishrak@iut-dhaka.edu>
2023-05-22 17:49:41 +0600

    reg student

---
+++ app/src/main/java/com/example/dormease/RegisterActivity.java
@@ -0,0 +1,161 @@
+package com.example.dormease;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthEmailException;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText editTextRegName, editTextRegEmail, editTextRegPassword,
+            editTextRegStid, editTextRegRoomno;
+    private ProgressBar progressBar;
+    private RadioGroup radioGroupRegBuilding;
+    private RadioButton radioButtonRegBuildingSelected;
+//    private static final String TAG = "RegisterActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+//        getSupportActionBar().setTitle("Register");
+
+        Toast.makeText(RegisterActivity.this, "Create account", Toast.LENGTH_LONG).show();
+
+        progressBar = findViewById(R.id.progress_bar);
+        editTextRegName = findViewById(R.id.edittext_reg_fullname);
+        editTextRegEmail = findViewById(R.id.edittext_reg_email);
+        editTextRegStid = findViewById(R.id.edittext_reg_stid);
+        editTextRegPassword = findViewById(R.id.edittext_reg_password);
+        editTextRegRoomno = findViewById(R.id.edittext_reg_roomno);
+        radioGroupRegBuilding = findViewById(R.id.radiogroup_reg_building);
+        radioGroupRegBuilding.clearCheck();
+
+        Button buttonReg = findViewById(R.id.regbtn);
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int selectedBuildingID = radioGroupRegBuilding.getCheckedRadioButtonId();
+                radioButtonRegBuildingSelected = findViewById(selectedBuildingID);
+
+                String textname = editTextRegName.getText().toString();
+                String textemail = editTextRegEmail.getText().toString();
+                String textstid = editTextRegStid.getText().toString();
+                String textpass = editTextRegPassword.getText().toString();
+                String textbuilding; //cant obtain value before checking if any radio btn selected or not
+                String textroomno = editTextRegRoomno.getText().toString();
+
+                if(TextUtils.isEmpty(textname)){
+                    Toast.makeText(RegisterActivity.this, "Please enter full name", Toast.LENGTH_LONG).show();
+                    editTextRegName.setError("Full name required");
+                    editTextRegName.requestFocus();
+                }else if(TextUtils.isEmpty(textemail)){
+                    Toast.makeText(RegisterActivity.this, "Please enter email", Toast.LENGTH_LONG).show();
+                    editTextRegEmail.setError("Email required");
+                    editTextRegEmail.requestFocus();
+                }
+                else if(!textemail.matches("^\\S+@iut-dhaka\\.edu$")){
+                    Toast.makeText(RegisterActivity.this, "Please re-enter email", Toast.LENGTH_LONG).show();
+                    editTextRegEmail.setError("Valid Email required");
+                    editTextRegEmail.requestFocus();
+                }
+                else if(TextUtils.isEmpty(textstid)){
+                    Toast.makeText(RegisterActivity.this, "Please enter student ID", Toast.LENGTH_LONG).show();
+                    editTextRegStid.setError("Student ID required");
+                    editTextRegStid.requestFocus();
+                } else if(TextUtils.isEmpty(textpass)){
+                    Toast.makeText(RegisterActivity.this, "Please enter password", Toast.LENGTH_LONG).show();
+                    editTextRegPassword.setError("Password required");
+                    editTextRegPassword.requestFocus();
+                } else if(textpass.length() < 6){
+                    Toast.makeText(RegisterActivity.this, "Password should be at least 6 characters", Toast.LENGTH_LONG).show();
+                    editTextRegPassword.setError("Password should be at least 6 characters");
+                    editTextRegPassword.requestFocus();
+                }else if(radioGroupRegBuilding.getCheckedRadioButtonId() == -1){
+                    Toast.makeText(RegisterActivity.this, "Please select building", Toast.LENGTH_LONG).show();
+                    radioButtonRegBuildingSelected.setError("Select your building");
+                    radioButtonRegBuildingSelected.requestFocus();
+                } else if(TextUtils.isEmpty(textroomno)) {
+                    Toast.makeText(RegisterActivity.this, "Enter Room number", Toast.LENGTH_LONG).show();
+                    editTextRegRoomno.setError("Room number is required");
+                    editTextRegRoomno.requestFocus();
+                }
+                else //everything ok
+                {
+                    textbuilding = radioButtonRegBuildingSelected.getText().toString();
+                    progressBar.setVisibility(View.VISIBLE);
+                    registerUser(textname,textemail,textstid,textbuilding,textroomno,textpass);
+                }
+            }
+        });
+
+    }
+
+    private void registerUser(String textname, String textemail, String textstid, String textbuilding, String textroomno, String textpass) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.createUserWithEmailAndPassword(textemail,textpass).addOnCompleteListener(RegisterActivity.this,
+                new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    System.out.println("ok");
+                    Toast.makeText(RegisterActivity.this, "User registered successfully", Toast.LENGTH_LONG).show();
+                    FirebaseUser firebaseUser = auth.getCurrentUser();
+
+                    //send verification email
+                    firebaseUser.sendEmailVerification();
+
+                    //open login page after successful reg
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    //to prevent user from returning back to register activity after registration
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                    startActivity(intent);
+                    finish(); //close register activity
+                }
+//                else{
+//                    // If reg fails, display a message to the user.
+//                    try {
+//                        throw task.getException();
+//                    }catch (FirebaseAuthEmailException e){
+//                        editTextRegEmail.setError("jhamela");
+//                        editTextRegEmail.requestFocus();
+//                    }catch (FirebaseAuthInvalidCredentialsException e){
+//                        editTextRegEmail.setError("jhamela2");
+//                        editTextRegEmail.requestFocus();
+//                    }catch (FirebaseAuthUserCollisionException e){
+//                        editTextRegEmail.setError("Already registered user");
+//                        editTextRegEmail.requestFocus();
+//                    }catch (Exception e){
+//                        Log.e(TAG, e.getMessage());
+//                        Toast.makeText(RegisterActivity.this, e.getMessage(),Toast.LENGTH_LONG).show();
+//
+//                    }
+//                }
+            }
+        });
+    }
+}

3b8fecb4d34262d5b4ea721fd9c9e5e2980f8e5f
Isaba Ishrak <isabaishrak@iut-dhaka.edu>
2023-05-24 03:41:17 +0600

    login

--- app/src/main/java/com/example/dormease/LoginActivity.java
+++ app/src/main/java/com/example/dormease/LoginActivity.java
@@ -1,14 +1,104 @@
 package com.example.dormease;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextLoginEmail, editTextLoginPass;
+    private ProgressBar progressBar;
+    private FirebaseAuth authProfile;
+    private static final String TAG = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+//        getSupportActionBar().setTitle("Login");
+
+        editTextLoginEmail = findViewById(R.id.edittext_login_email);
+        editTextLoginPass = findViewById(R.id.edittext_login_pass);
+        progressBar = findViewById(R.id.progress_bar);
+
+        //to check if user already logged in or not
+        authProfile = FirebaseAuth.getInstance();
+
+        Button buttonLogin = findViewById(R.id.loginbtn);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textemail = editTextLoginEmail.getText().toString();
+                String textpass = editTextLoginPass.getText().toString();
+
+                if(TextUtils.isEmpty(textemail)){
+                    Toast.makeText(LoginActivity.this, "Please enter email address", Toast.LENGTH_SHORT).show();
+                    editTextLoginEmail.setError("Email required");
+                    editTextLoginEmail.requestFocus();
+                }
+                else if(TextUtils.isEmpty(textpass)){
+                    Toast.makeText(LoginActivity.this, "Please enter password", Toast.LENGTH_SHORT).show();
+                    editTextLoginPass.setError("Password required");
+                    editTextLoginPass.requestFocus();
+                }
+                else
+                {
+                    progressBar.setVisibility(View.VISIBLE);
+                    loginUser(textemail,textpass);
+                }
+            }
+        });
+
+    }
+
+    private void loginUser(String textemail, String textpass) {
+
+        authProfile.signInWithEmailAndPassword(textemail,textpass).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(LoginActivity.this, "You are logged in", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    try {
+                        throw task.getException();
+                    }
+                    catch (FirebaseAuthInvalidUserException e)
+                    {
+                        editTextLoginEmail.setError("User does not exist or no longer valid. Please register again.");
+                        editTextLoginEmail.requestFocus();
+                    }
+                    catch (FirebaseAuthInvalidCredentialsException e) {
+                        editTextLoginEmail.setError("Invalid credentials. Kindly check and re-enter.");
+                        editTextLoginEmail.requestFocus();
+                    }
+                    catch (Exception e)
+                    {
+                        Log.e(TAG, e.getMessage());
+                        Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+        });
     }
 }
